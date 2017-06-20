115fe258046ab00a49dc8686173e6682d44286a3
ccmcwolf <ccmcwolf@gmail.com>
2017-06-21 02:34:47 +0530

    Store settings to merchant added

--- Project/Mart/src/main/java/com/zambrone/controller/DefaultPageController.java
+++ Project/Mart/src/main/java/com/zambrone/controller/DefaultPageController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by Chamith on 13/06/2017.
  */
@@ -30,5 +33,10 @@ public class DefaultPageController {
         return "login";
     }
 
+    @GetMapping(path = "/logout")
+    public String logout(HttpServletRequest request) throws ServletException {
+        request.logout();
+        return "store";
+    }
 
 }
